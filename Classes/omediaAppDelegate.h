4b2a8aed9e00e3f5c33145bcfcad46325dc33edc
xuhongfeng <life.xhf@gmail.com>
2011-09-12 09:17:59 +0800

    ADD : 新建项目

---
+++ Classes/omediaAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  omediaAppDelegate.h
+//  omedia
+//
+//  Created by xu hongfeng on 11-9-12.
+//  Copyright 2011 THU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface omediaAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  omedia
+//
+//  Created by xu hongfeng on 11-9-12.
+//  Copyright 2011 THU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
